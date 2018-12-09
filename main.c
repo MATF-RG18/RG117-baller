71d0854332a8c47cdbf335e66076be98c3a76ad5
gavrilovicm <gavrilovicmiodrag4@gmail.com>
2018-12-09 01:20:33 +0100

    Dodata kontrola kretanja levo i desno sa kamerom koja prati pomeranje

--- main.c
+++ main.c
@@ -3,15 +3,22 @@
 #include<math.h>
 #include<stdbool.h>
 #include<stdio.h>
+#include<time.h>
+
 
 #define pi 3.141592653589793
 double jump = 0;
 double move = 0;
 
 bool ball_jump= false;
-bool ball_move = false;
+bool ball_move_r= false;
+bool ball_move_l= false;
 float br = 0;
-int i = 2;
+
+//i - promenljiva koja odredjuje x koordinatu crtanja poligona
+int i = 0;
+// promenljive koje odredjuju koordinate crtanja parova prepreka
+int prvi = 3, drugi = 7, treci = 4, cetvrti = 8;
 
 
 /* Deklaracije callback funkcija. */
@@ -20,8 +27,12 @@ static void on_reshape(int width, int height);
 static void on_display(void);
 void specialInput(int key, int x, int y);
 void ball_jump_f(int value);
-void specialInput(int key, int x, int y);
 void ball_move_r_f(int value);
+void ball_move_l_f(int value);
+void set_new_objects();
+/* funkcija koja registruje kada se pusti taster na tastaturi koji smo drzali */
+void keyboard_up(unsigned char key, int x, int y);
+
 
 void draw_cube();
 void draw_floor_1();
@@ -44,11 +55,18 @@ int main(int argc, char **argv)
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
 	glutIdleFunc(on_display);
+	glutKeyboardUpFunc(keyboard_up);
 
-	/* funkcija koja dozvoljava hvatanje specijalnih tastera
-	 * tastature, kao sto su strelice */
-	glutSpecialFunc(specialInput);
+	/* 
+	 * funkcija koja ne dozvoljava ponavljanje karaktera koji drzimo
+	 * na ulazu, registruje ga samo jednom.
+	 */
+	glutIgnoreKeyRepeat(1);
 
+
+	/* Na pocetku kretanje loptice levo i desno je false */
+	ball_move_r = false;
+	ball_move_l = false;
 
     /* Obavlja se OpenGL inicijalizacija. */
     glClearColor(0.75, 0.75, 0.75, 0);
@@ -74,12 +92,39 @@ static void on_keyboard(unsigned char key, int x, int y)
 			glutTimerFunc(30, ball_jump_f, 5);
 			ball_jump = true;
 		break;
+	case 'l':
+		if (!ball_move_r){
+			glutTimerFunc(20, ball_move_r_f, 1);
+			ball_move_r = true;
+		}
+		break;
+	case 'j':
+		if (!ball_move_l){
+			glutTimerFunc(20, ball_move_l_f, 2);
+			ball_move_l = true;
+		}
+		break;
     }
-		
+}
+
+/*
+ * funkcija koja registruje pustanje tastera sa tastature.
+ * Kada se to desi hocemo da prekinemo animaciju.
+ */
+void keyboard_up(unsigned char key, int x, int y){
+	(void)x;
+	(void)y;
+	switch (key){
+	case 'l':
+		ball_move_r = false;
+		break;
+	case 'j':
+		ball_move_l = false;
+		break;
+	}
 }
 
 //funkcija za implementaciju skoka loptice 
-
 void ball_jump_f(int value){
 	if (value != 5) return;
 
@@ -101,45 +146,52 @@ void floor_move_period(void){
 	//funkcija za pomeranje podloge
 
 	if (br >= 0.7){
+		/*
+		 * Podlogu transliramo ulevo samo ako se sama loptica
+		 * pomera udesno i presla je polovinu ekrana
+		 */
+		if (ball_move_r){
+			glPushMatrix();
+			glMatrixMode(GL_PROJECTION);
+			glTranslatef(-0.02, 0, 0);
+			glPopMatrix();
 
-		glPushMatrix();
-		glMatrixMode(GL_PROJECTION);
-		glTranslatef(-0.01, 0, 0);
-		glPopMatrix();
-
-		//  povecavamo move za 0.01 kako bi loptica ostala 
-		//  u istoj poziciji
-		move += 0.01;
+			//  povecavamo move za 0.02 kako bi loptica ostala 
+			//  u istoj poziciji
+			move += 0.02;
+		}
 
 		/*
 		 * ako move dodje u celobrojnu vrednost i ta celobrojna vrednost je
-		 * deljiva sa 4 treba da pomerimo poligon udesno
+		 * deljiva sa 6(duzinu polovine poligona) treba da pomerimo poligon udesno
 		 */
-		if (((move - (int)move) < 0.01)&&((int)(move) % 4 == 0)){ 
+		if (((move - (int)move) < 0.02)&&((int)(move+6) % 6 == 0)){ 
 			//i - promenljiva koja odredjuje x koordinatu crtanja poligona
-			i+=4;
+			i+=6;
+			set_new_objects();
 		}
-
 	}else{
-		br += 0.01;
+		br += 0.02;
 	}
-
-
 }
+
 void ball_move_r_f(int value){
 	if (value != 1) return;
 
-	if (move <= 0.7) {
+	glutPostRedisplay();
+
+	if (br <= 0.7) {
 		/*
 		 * dokle god loptica ne dodje u polozaj da kamera treba
 		 * da je prati, izvrsavamo samo pomeranje loptice
 		 */
-		move += 0.01;
-		br = br + 0.01;
-		if (ball_move)
-			glutTimerFunc(30,ball_move_r_f, 1);
+		move += 0.02;
+		br = br + 0.02;
+		if (ball_move_r){
+			glutTimerFunc(20,ball_move_r_f, 1);
+		}
 		else{
-			ball_move = false;
+			ball_move_r = false;
 		}
 	}
 	else {
@@ -149,47 +201,53 @@ void ball_move_r_f(int value){
 		 * a loptica stoji u mestu zajedno sa kamerom fiksirana
 		 */
 		floor_move_period();
-		br = br + 0.01;
-		if (ball_move)
-			glutTimerFunc(30,ball_move_r_f, 1);
+		br = br + 0.02;
+		if (ball_move_r){
+			glutTimerFunc(20,ball_move_r_f, 1);
+		}
 		else{
-			ball_move = false;
+			ball_move_r = false;
 		}
 	}
-
-	glutPostRedisplay();
-
 
 }
 
-void specialInput(int key, int x, int y){
-	/*
-	 * funkcija za registrovanje tastera strelice.
-	 * Na strelicu se zapocinje igrica
-	 */
+void ball_move_l_f(int value){
+	if (value != 2) return;
 
-    switch (key) {
-	/* kretanje udesno */
-	case GLUT_KEY_RIGHT:
-	//	move += 0.01;
-		if (!ball_move){
-			glutTimerFunc(30, ball_move_r_f, 1);
-			ball_move = true;
-		}
-		break;
-	/* 
-	 * kretanje ulevo TODO
-	 * ---------------------
-	 *  za sada ne diram, kasnije ce imati svrhu u kodu 
-	 *  koji cu tek implementirati
-	 * */
-	case GLUT_KEY_LEFT:
-		move -= 0.01;
-		glutPostRedisplay();
-		break;
-
-	}
 	glutPostRedisplay();
+
+	if (br <= 0.7) {
+		/*
+		 * dokle god loptica ne dodje u polozaj da kamera treba
+		 * da je prati, izvrsavamo samo pomeranje loptice
+		 */
+		move -= 0.02;
+		br = br - 0.02;
+		if (ball_move_l){
+			glutTimerFunc(20,ball_move_l_f, 2);
+		}
+		else{
+			ball_move_l = false;
+		}
+	}
+	else {
+		/*
+		 * Kada loptica dodje u odredjenu poziciju, kamera pocinje
+		 * da je "prati", i poziva se funkcija za kretanje podloge u levo
+		 * a loptica stoji u mestu zajedno sa kamerom fiksirana
+		 */
+		floor_move_period();
+		br = 0.7 - 0.02;
+		move -= 0.02;
+		if (ball_move_l){
+			glutTimerFunc(20,ball_move_l_f, 2);
+		}
+		else{
+			ball_move_l = false;
+		}
+	}
+
 }
 
 static void on_reshape(int width, int height)
@@ -215,8 +273,51 @@ void draw_floor_2(){
 
 	glBegin(GL_POLYGON);
 		glNormal3f(0,1,0);
-		glVertex3f(i,   -0.05, -0.2);
-		glVertex3f(i,   -0.05,  0.2);
+		glVertex3f(i+4 , -0.05, -0.2);
+		glVertex3f(i+4 , -0.05,  0.2);
+		glVertex3f(i+10, -0.05,  0.2);
+		glVertex3f(i+10, -0.05, -0.2);
+	glEnd();
+	
+	/* crtanje prednje ivice podloge*/
+	glBegin(GL_POLYGON);
+		glNormal3f(0,0,-1);
+		glVertex3f(i+4 , -0.05, 0.2 );
+		glVertex3f(i+4 ,    -5, 0.2 );
+		glVertex3f(i+10,    -5, 0.2 );
+		glVertex3f(i+10, -0.05, 0.2 );
+	glEnd();
+
+	//iscrtavanje prepreka 
+	glPushMatrix();
+		glTranslatef(i+((float)prvi*(1/10.0)) + 1,0.1,0);
+		draw_cube();
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(i+((float)drugi*(1/10.0)) + 2,0.25,0);
+		draw_cube();
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(i+((float)treci*(1/10.0)) + 3,0.2,0);
+		draw_cube();
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(i+(float)cetvrti*(1/10.0) + 4,0.3,0);
+		glScalef(2,1,1);
+		draw_cube();
+	glPopMatrix();
+}
+	
+void draw_floor_1(){
+
+	/* Crtamo podlogu */
+	glBegin(GL_POLYGON);
+		glNormal3f(0,1,0);
+		glVertex3f(i-2, -0.05, -0.2);
+		glVertex3f(i-2, -0.05,  0.2);
 		glVertex3f(i+4, -0.05,  0.2);
 		glVertex3f(i+4, -0.05, -0.2);
 	glEnd();
@@ -224,56 +325,24 @@ void draw_floor_2(){
 	/* crtanje prednje ivice podloge*/
 	glBegin(GL_POLYGON);
 		glNormal3f(0,0,-1);
-		glVertex3f(i  , -0.05, 0.2 );
-		glVertex3f(i  ,    -5, 0.2 );
+		glVertex3f(i-2, -0.05, 0.2 );
+		glVertex3f(i-2,    -5, 0.2 );
 		glVertex3f(i+4,    -5, 0.2 );
 		glVertex3f(i+4, -0.05, 0.2 );
 	glEnd();
 
 }
 
-void draw_floor_1(){
+void set_new_objects(){
+	/* postavljamo seme za random funkciju */
+	srand(time(NULL));
 
-	/* Crtamo podlogu */
-	glBegin(GL_POLYGON);
-		glNormal3f(0,1,0);
-		glVertex3f(i-3, -0.05, -0.2);
-		glVertex3f(i-3, -0.05,  0.2);
-		glVertex3f(i  , -0.05,  0.2);
-		glVertex3f(i  , -0.05, -0.2);
-	glEnd();
-	
-	/* crtanje prednje ivice podloge*/
-	glBegin(GL_POLYGON);
-		glNormal3f(0,0,-1);
-		glVertex3f(i-3, -0.05, 0.2 );
-		glVertex3f(i-3,    -5, 0.2 );
-		glVertex3f(i  ,    -5, 0.2 );
-		glVertex3f(i  , -0.05, 0.2 );
-	glEnd();
-
-	/*
-	 * Par primera prepreka
-	 *
-	 * TODO : implementirati dinamicko generisanje prepreka
-	 */
-	glPushMatrix();
-		glTranslatef(2,0.1,0);
-		draw_cube();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(1.3,0.2,0);
-		draw_cube();
-	glPopMatrix();
-
-	glPushMatrix();
-		glTranslatef(1.9,0.1,0);
-		glScalef(2,1,1);
-		draw_cube();
-	glPopMatrix();
+	int k = rand();
+	prvi = k%10;
+	drugi = (k/10)%10;
+	treci = (k/100)%10;
+	cetvrti = (k/1000)%10;
 }
-
 
 void draw_sphere(){
 	/* ambijentalna refleksija za sferu*/
@@ -285,7 +354,7 @@ void draw_sphere(){
 	/* spekularna refleksija za sferu*/
 	GLfloat specular_lopta[] = { 1, 1, 1, 1 };           
 
-	GLfloat shininess = 60;
+	GLfloat shininess = 100;
 
 	glPushMatrix();
 	/* crtamo sferu */
@@ -299,7 +368,6 @@ void draw_sphere(){
     glutSolidSphere(0.05, 50,50);
 	
 	glPopMatrix();
-
 }
 
 
@@ -325,7 +393,6 @@ static void on_display(void)
 	/* Spekularna boja svetla. */                         
 	GLfloat light_specular[] = { 1, 1, 1, 1 };      
 
-
 	// ----------------------------------------------------------
 
 	/* ambijentalna refleksija za poligon */
@@ -339,7 +406,7 @@ static void on_display(void)
 
 
 	/* Koeficijent glatkosti materijala. */               
-	GLfloat shininess = 60;
+	GLfloat shininess = 100;
 
 	/* postavljanje svetla 0*/
 	glEnable(GL_LIGHT0);
